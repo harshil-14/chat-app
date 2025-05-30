5859533abded89bc701b273848b676f6de657388
harshil-14 <harshilmotivarash@gmail.com>
2025-05-30 10:26:47 +0530

    added signup route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,48 @@
-export const signup=(req,res)=>{
-    res.send("signup route");
+import User from "../models/user.model.js";
+import becrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const signup=async(req,res)=>{
+    const {fullName,email,password}=req.body;
+    try {
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        if (password.length<6) {
+            return res.status(400).json({message:"password must be atleat 6 characters"});
+        } 
+
+        const user=await User.findOne({email})
+        if(user) return res.status(400).json({message:"Email already exists"});
+
+        const salt=await becrypt.genSalt(10);
+        const hashedPassword=await becrypt.hash(password,salt);
+
+        const newUser=new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            generateToken(newUser._id,res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+            });
+        }
+        else{
+            res.status(400).json({message:"Invalid user Data"});
+        }
+    } catch (error) {
+        console.log("Error in signup controller",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
 };
 export const login=(req,res)=>{
     res.send("login route");
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,15 @@ import {connectDb} from './lib/db.js'
 
 dotenv.config();
 const app=express();
+app.use(express.json());
 const port=process.env.PORT;
 
 app.use("/api/auth",authRoutes)
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 
 app.listen(port, ()=>{
     console.log(`server running at http://localhost:${port}`)
