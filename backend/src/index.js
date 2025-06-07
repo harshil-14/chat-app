ef1029eb3cefc0d050eb70c513d3f86f797af968
harshil-14 <harshilmotivarash@gmail.com>
2025-06-07 16:38:13 +0530

    added message.controller.js message.models.js and message.route.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import {connectDb} from './lib/db.js'
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app=express();
 
+const port=process.env.PORT;
+
 app.use(express.json());
 app.use(cookieParser());
 
-
-const port=process.env.PORT;
-
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running');
