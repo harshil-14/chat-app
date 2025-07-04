1d6fe2eff8e95f14a45f7d4fed0e68fa02a1e2f6
harshil-14 <harshilmotivarash@gmail.com>
2025-07-04 12:38:27 +0530

    fixed bug in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {connectDb} from './lib/db.js'
+import { connectDb } from './lib/db.js'
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
-import {app,server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 // const app=express();
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 const __dirname = path.resolve();
 
 
@@ -23,20 +23,22 @@ app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 // app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
+  origin: "http://localhost:5173",
+  credentials: true
 }));
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
-if (process.env.NODE_ENV==="production") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-  })
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
 }
 
 app.get('/', (req, res) => {
@@ -44,7 +46,7 @@ app.get('/', (req, res) => {
 });
 
 
-server.listen(port, ()=>{
-    console.log(`server running at http://localhost:${port}`)
-    connectDb();
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`)
+  connectDb();
 })
